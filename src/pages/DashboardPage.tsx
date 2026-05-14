2f7a5e45f1d0ea66179ff802628436f15f75e901
max <maksimas.volodascik@gmail.com>
2026-05-14 23:51:29 +0300

    feat: add mid row for statistics in dashboard page

--- src/pages/DashboardPage.tsx
+++ src/pages/DashboardPage.tsx
@@ -1,8 +1,35 @@
-import { Box, Card, Typography } from "@mui/material";
+import {
+  Box,
+  Card,
+  Paper,
+  Table,
+  TableContainer,
+  TableHead,
+  TableRow,
+  Typography,
+} from "@mui/material";
 import { PageLayout } from "../components/layout/PageLayout";
 import Sidebar from "../components/Sidebar";
 import { ContentLayout } from "../components/layout/ContentLayout";
-import Button from "../components/Button";
+import {
+  Pie,
+  PieChart,
+  ResponsiveContainer,
+  Sector,
+  type PieSectorShapeProps,
+} from "recharts";
+
+const data = [
+  { name: "Active", value: 72 },
+  { name: "Inactive", value: 32 },
+  { name: "Never connected", value: 12 },
+];
+
+const colors = ["#e24646", "#ec7424", "#413c35"];
+
+const MyCustomPie = (props: PieSectorShapeProps) => (
+  <Sector {...props} fill={colors[props.index % colors.length]} />
+);
 
 export default function DashboardPage() {
   return (
@@ -23,7 +50,7 @@ export default function DashboardPage() {
                 background: "#1a1917",
                 borderRadius: "1px solid #0a0a09",
                 position: "relative",
-                padding: "16px",
+                padding: "20px",
               }}
             >
               <Typography
@@ -69,7 +96,7 @@ export default function DashboardPage() {
                 background: "#1a1917",
                 borderRadius: "1px solid #0a0a09",
                 position: "relative",
-                padding: "16px",
+                padding: "20px",
               }}
             >
               <Typography
@@ -115,7 +142,7 @@ export default function DashboardPage() {
                 background: "#1a1917",
                 borderRadius: "1px solid #0a0a09",
                 position: "relative",
-                padding: "16px",
+                padding: "20px",
               }}
             >
               <Typography
@@ -161,7 +188,7 @@ export default function DashboardPage() {
                 background: "#1a1917",
                 borderRadius: "1px solid #0a0a09",
                 position: "relative",
-                padding: "16px",
+                padding: "20px",
               }}
             >
               <Typography
@@ -202,6 +229,194 @@ export default function DashboardPage() {
                 added this week
               </Typography>
             </Card>
+          </Box>
+
+          <Box
+            sx={{
+              padding: "0px 30px 30px 30px",
+              display: "grid",
+              gridTemplateColumns: "3fr 3fr 2fr",
+              gap: "14px",
+            }}
+          >
+            <Card
+              sx={{
+                background: "#1a1917",
+                borderRadius: "1px solid #0a0a09",
+                position: "relative",
+                padding: "20px",
+              }}
+            >
+              <Box>
+                <Typography
+                  sx={{
+                    fontSize: "16px",
+                    color: "#fd8636",
+                    fontWeight: "bold",
+                  }}
+                >
+                  Device Activity Status
+                </Typography>
+                <Typography
+                  sx={{
+                    fontSize: "12px",
+                    color: "#8a8580",
+                    fontWeight: "bold",
+                  }}
+                >
+                  Across all registered devices
+                </Typography>
+              </Box>
+              <Box
+                sx={{ display: "flex", alignItemstems: "center", gap: "32px" }}
+              >
+                <Box
+                  sx={{
+                    marginTop: "20px",
+                    width: 180,
+                    height: 180,
+                    display: "flex",
+                    alignItems: "center",
+                    justifyContent: "center",
+                  }}
+                >
+                  <ResponsiveContainer width="100%" height="100%">
+                    <PieChart>
+                      <Pie
+                        data={data}
+                        dataKey="value"
+                        startAngle={90}
+                        endAngle={-270}
+                        innerRadius={65}
+                        outerRadius={85}
+                        paddingAngle={5}
+                        cornerRadius={8}
+                        stroke="none"
+                        shape={MyCustomPie}
+                      ></Pie>
+                      <text
+                        y="50%"
+                        textAnchor="middle"
+                        dominantBaseline="middle"
+                      >
+                        <tspan
+                          x="50%"
+                          dy="-0.2em"
+                          fontSize="42"
+                          fontWeight="bold"
+                          fill="#e2e2e2"
+                        >
+                          {100}
+                        </tspan>
+                        <tspan
+                          x="50%"
+                          dy="2em"
+                          fontSize="14"
+                          fontWeight="500"
+                          fill="#8a8580"
+                        >
+                          Total
+                        </tspan>
+                      </text>
+                    </PieChart>
+                  </ResponsiveContainer>
+                </Box>
+                <Box
+                  sx={{
+                    display: "flex",
+                    flexDirection: "column",
+                    paddingTop: "30px",
+                    gap: "10px",
+                  }}
+                >
+                  <Box
+                    sx={{ display: "flex", alignItems: "center", gap: "10px" }}
+                  >
+                    <Box
+                      sx={{
+                        width: "10px",
+                        height: "10px",
+                        borderRadius: "3px",
+                        backgroundColor: "#e24646",
+                      }}
+                    ></Box>
+                    <Box sx={{ fontSize: "13px", color: "#e2e2e2" }}>
+                      Active today
+                    </Box>
+                    <Box
+                      sx={{
+                        fontSize: "13px",
+                        fontWeight: "bold",
+                        color: "#e2e2e2",
+                      }}
+                    >
+                      72
+                    </Box>
+                  </Box>
+                  <Box
+                    sx={{ display: "flex", alignItems: "center", gap: "10px" }}
+                  >
+                    <Box
+                      sx={{
+                        width: "10px",
+                        height: "10px",
+                        borderRadius: "3px",
+                        backgroundColor: "#ec7424",
+                      }}
+                    ></Box>
+                    <Box sx={{ fontSize: "13px", color: "#e2e2e2" }}>
+                      Offline ({">"}24 hours)
+                    </Box>
+                    <Box
+                      sx={{
+                        fontSize: "13px",
+                        fontWeight: "bold",
+                        color: "#e2e2e2",
+                      }}
+                    >
+                      72
+                    </Box>
+                  </Box>
+                  <Box
+                    sx={{ display: "flex", alignItems: "center", gap: "10px" }}
+                  >
+                    <Box
+                      sx={{
+                        width: "10px",
+                        height: "10px",
+                        borderRadius: "3px",
+                        backgroundColor: "#413c35",
+                      }}
+                    ></Box>
+                    <Box sx={{ fontSize: "13px", color: "#e2e2e2" }}>
+                      Never connected
+                    </Box>
+                    <Box
+                      sx={{
+                        fontSize: "13px",
+                        fontWeight: "bold",
+                        color: "#e2e2e2",
+                      }}
+                    >
+                      72
+                    </Box>
+                  </Box>
+                </Box>
+              </Box>
+            </Card>
+            <TableContainer
+              component={Paper}
+              sx={{ width: "100%", background: "#1a1917" }}
+            >
+              <Table sx={{ minWidth: 100 }} aria-label="simple table">
+                <TableHead>
+                  <TableRow>
+                    Table for devices and total messages sent today
+                  </TableRow>
+                </TableHead>
+              </Table>
+            </TableContainer>
+            <Box sx={{ width: "100%", background: "#1a1917" }}></Box>
           </Box>
         </ContentLayout>
       </PageLayout>
