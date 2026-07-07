9a7329e9bfa384490625fa77ae56119743b1d8fe
max <maksimas.volodascik@gmail.com>
2026-07-07 22:04:59 +0300

    fix: Move ThemeProvider out of main.tsx into App.tsx so changing   theme colors no longer forces a full page reload

--- src/App.tsx
+++ src/App.tsx
@@ -1,27 +1,32 @@
 import { Routes, Route } from "react-router-dom";
 import LoginPage from "./pages/LoginPage";
-
 import RegisterPage from "./pages/RegisterPage";
 import DashboardPage from "./pages/DashboardPage";
 import { PrivateAccess, PublicAccess } from "./services/authProvider";
 import { PageLayout } from "./components/layout/PageLayout";
 import DevicesPage from "./pages/DevicesPage";
+import UsersPage from "./pages/UsersPage";
+import { ThemeProvider } from "@mui/material";
+import { themeTemplate } from "./styles/theme";
 
 export const App = () => {
   return (
-    <Routes>
-      <Route element={<PublicAccess />}>
-        <Route path="/login" element={<LoginPage />} />
-        <Route path="/register" element={<RegisterPage />} />
-        <Route path="*" element={<RegisterPage />} />
-      </Route>
-      <Route element={<PrivateAccess />}>
-        <Route element={<PageLayout />}>
-          <Route path="/" element={<DashboardPage />} />
-          <Route path="/dashboard" element={<DashboardPage />} />
-          <Route path="/devices" element={<DevicesPage />} />
+    <ThemeProvider theme={themeTemplate}>
+      <Routes>
+        <Route element={<PublicAccess />}>
+          <Route path="/login" element={<LoginPage />} />
+          <Route path="/register" element={<RegisterPage />} />
+          <Route path="*" element={<RegisterPage />} />
         </Route>
-      </Route>
-    </Routes>
+        <Route element={<PrivateAccess />}>
+          <Route element={<PageLayout />}>
+            <Route path="/" element={<DashboardPage />} />
+            <Route path="/dashboard" element={<DashboardPage />} />
+            <Route path="/devices" element={<DevicesPage />} />
+            <Route path="/users" element={<UsersPage />} />
+          </Route>
+        </Route>
+      </Routes>
+    </ThemeProvider>
   );
 };
--- src/main.tsx
+++ src/main.tsx
@@ -1,22 +1,19 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { App } from "./App";
-import { CssBaseline, ThemeProvider } from "@mui/material";
+import { CssBaseline } from "@mui/material";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { BrowserRouter } from "react-router-dom";
-import { themeTemplate } from "./styles/theme";
 
 const queryClient = new QueryClient();
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <ThemeProvider theme={themeTemplate}>
-      <CssBaseline />
-      <QueryClientProvider client={queryClient}>
-        <BrowserRouter>
-          <App />
-        </BrowserRouter>
-      </QueryClientProvider>
-    </ThemeProvider>
+    <CssBaseline />
+    <QueryClientProvider client={queryClient}>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </QueryClientProvider>
   </StrictMode>,
 );
